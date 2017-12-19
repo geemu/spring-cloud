a57e58dc05f2d0198b9b99438dfd879385a333b4
陈方明 <cfmmail@sina.com>
2017-12-19 22:39:27 +0800

    2

---
+++ config-server/src/main/java/me/geemu/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package me.geemu.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Geemu
+ * Email: cfmmail@sina.com
+ * Date:
+ * Description:
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
