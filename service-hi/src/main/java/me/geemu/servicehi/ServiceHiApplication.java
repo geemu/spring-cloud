25b3fae4e641de561ee683de135003a42dd0662c
chenfangming <chenfangming@91zdan.com>
2017-12-26 18:04:41 +0800

    dada

---
+++ service-hi/src/main/java/me/geemu/servicehi/ServiceHiApplication.java
@@ -0,0 +1,20 @@
+package me.geemu.servicehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Geemu
+ * Email:cfmmail@sina.com
+ * Date: 2017/12/5 13:42
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+}
---
+++ service-hi/src/main/java/me/geemu/servicehi/controller/HiController.java
@@ -0,0 +1,23 @@
+package me.geemu.servicehi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Geemu
+ * Email:cfmmail@sina.com
+ * Date: 2017/12/26  15:56
+ * Description: user
+ */
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+}
