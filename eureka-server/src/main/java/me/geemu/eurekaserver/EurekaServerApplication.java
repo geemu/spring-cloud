34a8f499c7663208df4ce56d91f430202ffcc4b4
chenfangming <chenfangming@91zdan.com>
2017-12-26 15:51:18 +0800

    111

---
+++ eureka-server/src/main/java/me/geemu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package me.geemu.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Geemu
+ * Email:cfmmail@sina.com
+ * Date: 2017/12/5 13:42
+ * Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
