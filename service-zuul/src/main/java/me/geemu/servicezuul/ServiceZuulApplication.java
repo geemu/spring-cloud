fae8de84b2ab3b6df56860f6f710cdf06b9daaf8
chenfangming <chenfangming@91zdan.com>
2017-12-27 18:06:11 +0800

    添加zuul后断路由失效

---
+++ service-zuul/src/main/java/me/geemu/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,22 @@
+package me.geemu.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Geemu
+ * Email:cfmmail@sina.com
+ * Date: 2017/12/26  16:32
+ * Description: user
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+}
