9bafa5e4f4e74c48c3650b7748256fda206013d9
chenfangming <chenfangming@91zdan.com>
2017-12-27 18:19:38 +0800

    配置

--- config-server/src/main/java/me/geemu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/me/geemu/configserver/ConfigServerApplication.java
@@ -2,11 +2,19 @@ package me.geemu.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @Author: Geemu
+ * Email:cfmmail@sina.com
+ * Date: 2017/12/5 13:42
+ * Description:
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
