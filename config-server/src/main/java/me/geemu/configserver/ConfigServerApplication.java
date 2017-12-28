5fbc700b3cd5d3d5ce9582157e2d2247387a43af
chenfangming <chenfangming@91zdan.com>
2017-12-28 10:08:43 +0800

    配置服务器

--- config-server/src/main/java/me/geemu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/me/geemu/configserver/ConfigServerApplication.java
@@ -3,7 +3,6 @@ package me.geemu.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: Geemu
@@ -11,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * Date: 2017/12/5 13:42
  * Description:
  */
-@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
